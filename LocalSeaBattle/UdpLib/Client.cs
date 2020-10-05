0324c23046d9f9deb1eeec748c79f3da2f0a3284
Gonchar Alexis <alexgonchar00@gmail.com>
2020-10-05 16:49:36 +0300

    Create a library fo working with the udp protocol.

---
+++ LocalSeaBattle/UdpLib/Client.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace UDPLib
+{
+    /// <summary>
+    /// A class for communicating with another computer using the udp protocol.
+    /// </summary>
+    public class Client
+    {
+        private UdpClient receiver;
+        private string remoteAddress; // the addres to which we connect
+        private int remotePort; // the port to which we connect
+        private int localPort; // local port
+
+        public delegate void ReceiveHandler(string message);
+        public event ReceiveHandler Notify;
+        public Client(string ip, int remotePort, int localPort)
+        {
+            try
+            {
+                this.localPort = localPort;
+                remoteAddress = ip;
+                this.remotePort = remotePort;
+
+                Thread receiveThread = new Thread(new ThreadStart(ReceiveData));
+                receiveThread.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void SendData(string message)
+        {
+            UdpClient sender = new UdpClient(); // создаем UdpClient для отправки сообщений
+            try
+            {
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                sender.Send(data, data.Length, remoteAddress, remotePort); // отправка
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                sender.Close();
+            }
+        }
+
+        public void ReceiveData()
+        {
+            receiver = new UdpClient(localPort); // UdpClient для получения данных
+            IPEndPoint remoteIp = null; // адрес входящего подключения
+            try
+            {
+                while (true)
+                {
+                    byte[] data = receiver.Receive(ref remoteIp); // получаем данные
+                    string message = Encoding.Unicode.GetString(data);
+                    Notify(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                receiver.Close();
+            }
+        }
+
+        public void CloseClient()
+        {
+            if (receiver != null)
+            {
+                receiver.Close();
+            }
+        }
+    }
+}
